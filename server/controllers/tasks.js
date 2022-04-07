0f4235a54fbac9baf4fe362b3f1fa93856cfab54
Sam <sammccathie2@gmail.com>
2022-04-07 14:24:46 +1200

    Added update route.

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -31,8 +31,23 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("update task");
+const updateTask = async (req, res) => {
+  try {
+    const {id: taskID} = req.params;
+    // second arg (req.boy) is the new value
+    // third arg object.
+    // -  new: true will always return the new value instead of the old one
+    const Task = await task.findOneAndUpdate({_id: taskID}, req.body, {
+      new: true, // ensures that the new value is returned instead of the old one.
+      runValidators: true, // runs the evaluators from the TaskSchema -> without this someone could update the name to e.g. ""
+    });
+    if (!Task) {
+      return res.status(404).json({msg: `No task with id = ${taskID}`});
+    }
+    res.status(200).json(Task);
+  } catch (error) {
+    res.status(500).json({msg: error.errors.name.message});
+  }
 };
 
 const deleteTask = async (req, res) => {
